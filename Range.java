f13074fb7a2a67e14288812ebbc6e70fc5173144
pokiebob <ccursetjee@gmail.com>
2020-12-07 00:47:26 -0500

    constructor

---
+++ Range.java
@@ -0,0 +1,18 @@
+import java.util.NoSuchElementException;
+
+class Range implements IntegerSequence {
+
+	private int start, end, current;
+
+	public Range(int start, int end) {
+		if (end < start) {
+			throw new IllegalArgumentException(
+				"end value " + end + " cannot be less than start value " + start);
+		}
+		this.start = start;
+		this.end = end;
+		current = start;
+	}
+
+	
+}
