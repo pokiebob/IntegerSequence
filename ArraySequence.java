e6fb213522c9ff3718a7253aa6c2a2d690bf2c2f
pokiebob <ccursetjee@gmail.com>
2020-12-07 12:43:15 -0500

    hasNext

--- ArraySequence.java
+++ ArraySequence.java
@@ -8,7 +8,24 @@ public class ArraySequence implements IntegerSequence {
 		data = other;
 		currentIndex = 0;
 	}
-	
+
+	public boolean hasNext() {
+		return currentIndex <= data.length;
+	}
+
+	//does the sequence have more elements?
+	public int next() {
+
+	}       
+	//return the current value in the sequence and advances to the next element.
+	public int length() {
+
+	}     
+	//returns the total length of the sequence
+	public void reset() {
+
+	}     
+	//start over from the start of the sequence
 
 	public static void main(String[] args) {
 
